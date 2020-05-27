2d3f9a2448688c5554e784e81b73db150e9ac395
Mario Barbosa <marioabarbosam@gmail.com>
2020-05-27 02:10:14 -0500

    Carpeta y archivos creados

---
+++ solutions/mariobarbosa777/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+
+    def duplicate_zeros(self, arr: List[int]):
+        
+        for num in self.arr:
+            print (num)
+
+
+        # AQUÍ VA TU SOLUCIÓN
